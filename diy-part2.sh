9f938b2856e94989117d94a74c3b60a9fe420a4c
CWEIB <44942392+CWEIB@users.noreply.github.com>
2026-04-27 20:06:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,28 +19,6 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# 1. 删除所有可能导致冲突的内置 MTK 5G 驱动
-rm -rf package/mtk/applications/5g-modem
-
-# 2. 移除重复的工具定义
-rm -rf feeds/packages/utils/sms-tool
-rm -rf feeds/telephony/net/sendat
-
-./scripts/feeds update -a
-
-# 修复依赖
-find feeds/qmodem/ -name Makefile -exec sed -i 's/+kmod-mhi-wwan//g' {} +
-
-# 3. 移除 qmodem 中不使用的冗余 LuCI 插件（只留 next 版）
-# 这一步能大幅减少生成 ipk 时的磁盘占用和 js 文件冲突
-find feeds/qmodem/luci/ -maxdepth 1 -type d ! -name "luci-app-qmodem-next" -exec rm -rf {} +
-
-# 强制安装 QModem 包
-./scripts/feeds install -f -p qmodem qmodem qfirehose quectel_CM_5G_M luci-app-qmodem-next sms_forwarder_next
-
-# 安装其它包
-./scripts/feeds install -a
-
 # 临时解决Rust问题
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
