a63d2e911f8c40f4ba1c14b1f0b8b07f3c7f00cb
CWEIB <44942392+CWEIB@users.noreply.github.com>
2026-05-06 11:04:19 +0800

    Update IP address and install libfuse-dev

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+sudo apt install libfuse-dev
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
