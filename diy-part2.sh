e7713ea60bab9d1c245f71802b5c02d9535e051e
CWEIB <44942392+CWEIB@users.noreply.github.com>
2026-05-10 12:30:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -136,8 +136,8 @@ sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
 echo "🔧 Qmodem安装中..."
-./scripts/feeds update qmodem
-./scripts/feeds install -a -p qmodem
+# ./scripts/feeds update qmodem
+# ./scripts/feeds install -a -p qmodem
 # ./scripts/feeds update -a && ./scripts/feeds install -a
 ./scripts/feeds install -a -f -p qmodem
 # ./scripts/feeds update -a && ./scripts/feeds install -a
